7e242004c7f313ba5fc2bf4f8297432810086b6f
shCHO9801 <csh980116@naver.com>
2025-03-24 19:11:14 +0900

    초기 세팅
    초기 세팅

---
+++ boottalk/src/main/java/com/icandoit/boottalk/BoottalkApplication.java
@@ -0,0 +1,13 @@
+package com.icandoit.boottalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoottalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoottalkApplication.class, args);
+	}
+
+}
---
+++ boottalk/src/test/java/com/icandoit/boottalk/BoottalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icandoit.boottalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoottalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
