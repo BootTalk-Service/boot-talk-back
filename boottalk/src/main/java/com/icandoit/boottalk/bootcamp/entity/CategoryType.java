d9b265d1e1d977ae1c7210cac90cc77b07cc15af
shCHO9801 <csh980116@naver.com>
2025-03-25 19:41:56 +0900

    feat: CategoryType Enum 매핑 기능 추가
    - 한국어 카테고리 이름을 영어 Enum으로 변환하는 기능 추가 (fromKoreanName 메서드)
    - 영어 Enum 이름을 한국어로 변환하는 기능 유지 (getKoreanNameByEnglishName 메서드)
    - 잘못된 카테고리 이름 입력 시 예외 발생하도록 처리 (IllegalArgumentException)

--- boottalk/src/main/java/com/icandoit/boottalk/bootcamp/entity/CategoryType.java
+++ boottalk/src/main/java/com/icandoit/boottalk/bootcamp/entity/CategoryType.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum CategoryType {
+  //TODO : 추후에 카테고리들 더 추가 해야 함
   APPLICATION_SW_ENGINEERING("응용SW엔지니어링"),
   SMART_EQUIPMENT_DESIGN("스마트설비설계"),
   INFORMATION_SECURITY_MANAGEMENT("정보보호관리·운영"),
@@ -30,6 +31,9 @@ public enum CategoryType {
   }
 
   public static CategoryType fromKoreanName(String koreanName) {
+    if(!koreanToEnumMap.containsKey(koreanName)) {
+      throw new IllegalArgumentException("지원하지 않는 카테고리 이름입니다.");
+    }
     return koreanToEnumMap.get(koreanName);
   }
 }
