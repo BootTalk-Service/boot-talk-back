9c3faaecb0e5ece1b0c0d9c7c736f0ecd9ad7338
LeeGeonu <rjsdndl909@gmail.com>
2025-03-26 09:16:14 +0900

    feat : libs 기능구현 - Exception(global, custom) - SuccessResponse - BaseEntity

---
+++ boottalk/src/main/java/com/icandoit/boottalk/libs/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.icandoit.boottalk.libs.exception;
+
+import com.icandoit.boottalk.libs.dto.ExceptionResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * CustomException 처리 - ErrorCode에 정의된 예외 반환
+     */
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ExceptionResponseDto<Void>> handleCustomException(CustomException e) {
+        log.error("CustomException 발생: {}", e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+            .body(ExceptionResponseDto.of(e.getErrorCode().getHttpStatus(), e.getErrorCode().getMessage()));
+    }
+
+    /**
+     * RuntimeException 처리 - 서버 내부 오류 반환
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionResponseDto<Void>> handleRuntimeException(RuntimeException e) {
+        log.error("RuntimeException 발생: ", e);
+        return ResponseEntity.internalServerError()
+            .body(ExceptionResponseDto.of(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus(),
+                ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
+    }
+}
