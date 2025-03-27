b28dc8e4aa50b4cc0788e066c08be9913b6eb639
shCHO9801 <csh980116@naver.com>
2025-03-27 21:38:52 +0900

    feat: 부트 캠프 리스트 조회 기능 구현

--- boottalk/src/main/java/com/icandoit/boottalk/bootcamp/exception/BootcampErrorCode.java
+++ boottalk/src/main/java/com/icandoit/boottalk/bootcamp/exception/BootcampErrorCode.java
@@ -13,7 +13,9 @@ public enum BootcampErrorCode {
 
 	/* 404 NOT_FOUND */
 	BOOTCAMP_NOT_FOUND(404, "요청한 부트캠프를 찾을 수 없습니다."),
-	TRAINING_CENTER_NOT_FOUND(404, "요청한 훈련 기관을 찾을 수 없습니다.");
+	TRAINING_CENTER_NOT_FOUND(404, "요청한 훈련 기관을 찾을 수 없습니다."),
+	API_DATA_IS_EMPTY(404, "API 데이터가 비어 있습니다."),
+	DATA_FETCH_ERROR(404, "데이터 요청 중 오류가 발생했습니다.");
 
 	private final Integer httpStatus;
 	private final String message;
