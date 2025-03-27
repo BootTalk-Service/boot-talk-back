45bc83e539c32c45e41b7656553c314478b0f856
shCHO9801 <csh980116@naver.com>
2025-03-28 00:09:30 +0900

    feat: Bootcamp 정부 24 api 디테일 정보 요청 기능 추가

--- boottalk/src/main/java/com/icandoit/boottalk/bootcamp/service/Employ24ApiService.java
+++ boottalk/src/main/java/com/icandoit/boottalk/bootcamp/service/Employ24ApiService.java
@@ -1,5 +1,8 @@
 package com.icandoit.boottalk.bootcamp.service;
 
+import static com.icandoit.boottalk.bootcamp.dto.BootcampEmploy24Response.*;
+import static com.icandoit.boottalk.bootcamp.exception.BootcampErrorCode.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -13,9 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.icandoit.boottalk.bootcamp.dto.BootcampListResponseDto;
 import com.icandoit.boottalk.bootcamp.exception.BootcampCustomException;
-import com.icandoit.boottalk.bootcamp.exception.BootcampErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,8 +24,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Employ24ApiService {
 
-	private static final String LIST_API_URL = "https://www.work24.go.kr/cm/openApi/call/hr/callOpenApiSvcInfo310L01.do";
-	private static final String DETAIL_API_URL = "https://www.work24.go.kr/cm/openApi/call/hr/callOpenApiSvcInfo310L02.do";
+	private static final String HOST = "www.work24.go.kr";
+	private static final String LIST_API_PATH = "/cm/openApi/call/hr/callOpenApiSvcInfo310L01.do";
+	private static final String DETAIL_API_PATH = "/cm/openApi/call/hr/callOpenApiSvcInfo310L02.do";
+
 	private static final String AUTH_KEY = "646d5bd7-af0c-42b1-94db-fd6d2c6908f7";
 	private static final ObjectMapper objectMapper = new ObjectMapper();
 	private final RestTemplate restTemplate;
@@ -37,22 +40,27 @@ public class Employ24ApiService {
 	String endDateString = endDate.format(formatter);
 
 	public List<BootcampListResponseDto> fetchBootcampData(String creseTraceseSe, String srchNcs1) {
+		String url;
 
-		String url = UriComponentsBuilder.newInstance()
-			.scheme("https")
-			.host("www.work24.go.kr")
-			.path("/cm/openApi/call/hr/callOpenApiSvcInfo310L01.do")
-			.queryParam("authKey", AUTH_KEY)
-			.queryParam("returnType", "JSON")
-			.queryParam("outType", 1)
-			.queryParam("pageNum", 1)
-			.queryParam("pageSize", 100)
-			.queryParam("srchTraStDt", todayString)
-			.queryParam("srchTraEndDt", endDateString)
-			.queryParam("crseTracseSe", creseTraceseSe)
-			.queryParam("srchNcs1", srchNcs1)
-			.build()
-			.toUriString();
+		try {
+			url = UriComponentsBuilder.newInstance()
+				.scheme("https")
+				.host(HOST)
+				.path(LIST_API_PATH)
+				.queryParam("authKey", AUTH_KEY)
+				.queryParam("returnType", "JSON")
+				.queryParam("outType", 1)
+				.queryParam("pageNum", 1)
+				.queryParam("pageSize", 100)
+				.queryParam("srchTraStDt", todayString)
+				.queryParam("srchTraEndDt", endDateString)
+				.queryParam("crseTracseSe", creseTraceseSe)
+				.queryParam("srchNcs1", srchNcs1)
+				.build()
+				.toUriString();
+		} catch (Exception e) {
+			throw new BootcampCustomException(URL_BUILD_ERROR);
+		}
 
 		System.out.println("print URL : " + url);
 
@@ -64,35 +72,88 @@ public class Employ24ApiService {
 				String.class
 			);
 
-			if(!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
-				throw new BootcampCustomException(BootcampErrorCode.DATA_FETCH_ERROR);
+			if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
+				System.out.println("if : is not 2xx error");
+				throw new BootcampCustomException(DATA_FETCH_ERROR);
 			}
 
 			JsonNode rootNode = objectMapper.readTree(response.getBody());
 			JsonNode srchListNode = rootNode.get("srchList");
 			System.out.println("get srchList : " + rootNode.get("srchList"));
 
-			if(srchListNode.isEmpty()) {
-				throw new BootcampCustomException(BootcampErrorCode.API_DATA_IS_EMPTY);
+			if (srchListNode.isEmpty()) {
+				throw new BootcampCustomException(API_DATA_IS_EMPTY);
 			}
 
 			List<BootcampListResponseDto> bootcampDataList = new ArrayList<>();
 
-			for(JsonNode node : srchListNode) {
+			for (JsonNode node : srchListNode) {
 				try {
 					BootcampListResponseDto bootcampDto = objectMapper.treeToValue(node, BootcampListResponseDto.class);
 					bootcampDataList.add(bootcampDto);
 					System.out.println("파싱 성공 : " + bootcampDto.getBootcampName());
 				} catch (Exception e) {
-					System.out.println("파싱 실패 : " + e.getMessage());
+					throw new BootcampCustomException(DATA_PARSING_ERROR);
 				}
 			}
 
-			bootcampDataList.forEach(dto -> System.out.println(dto.getBootcampName()));
 			return bootcampDataList;
 
 		} catch (Exception e) {
-			throw new BootcampCustomException(BootcampErrorCode.DATA_FETCH_ERROR);
+			throw new BootcampCustomException(DATA_FETCH_ERROR);
+		}
+	}
+
+	public BootcampDetailResponseDto fetchBootcampDetail(String bootcampId, String bootcampDegree,
+		String trainingCenterId) {
+		String url;
+
+		try {
+			url = UriComponentsBuilder.newInstance()
+				.scheme("https")
+				.host(HOST)
+				.path(DETAIL_API_PATH)
+				.queryParam("authKey", AUTH_KEY)
+				.queryParam("returnType", "JSON")
+				.queryParam("outType", 2)
+				.queryParam("srchTrprId", bootcampId)
+				.queryParam("srchTrprDegr", bootcampDegree)
+				.queryParam("srchTorgId", trainingCenterId)
+				.build()
+				.toUriString();
+		} catch (Exception e) {
+			throw new BootcampCustomException(URL_BUILD_ERROR);
+		}
+
+		System.out.println("상세 페이지 URL: " + url);
+
+		try {
+			ResponseEntity<String> response = restTemplate.exchange(
+				url,
+				HttpMethod.GET,
+				null,
+				String.class
+			);
+
+			if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
+				throw new BootcampCustomException(DATA_FETCH_ERROR);
+			}
+
+			JsonNode rootNode = objectMapper.readTree(response.getBody());
+			JsonNode instBaseInfo = rootNode.get("inst_base_info");
+			JsonNode instDetailInfo = rootNode.get("inst_detail_info");
+
+			try {
+				BootcampDetailResponseDto detailResponseDto = objectMapper.treeToValue(
+					instBaseInfo, BootcampDetailResponseDto.class);
+				String govBusiNm = objectMapper.convertValue("govBusiNm", String.class);
+				detailResponseDto.setGovernmentBusinessName(govBusiNm);
+				return detailResponseDto;
+			} catch (Exception e) {
+				throw new BootcampCustomException(DATA_PARSING_ERROR);
+			}
+		} catch (Exception e) {
+			throw new BootcampCustomException(API_RESPONSE_ERROR);
 		}
 	}
 }
