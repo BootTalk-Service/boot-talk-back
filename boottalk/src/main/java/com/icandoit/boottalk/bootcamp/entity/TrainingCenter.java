baca801ddcb545322a8b3a3182be44fa221dbaf6
shCHO9801 <csh980116@naver.com>
2025-03-29 21:25:08 +0900

    refactor : 어노테이션 및 네이밍 수정 - Entity Column 어노테이션 제거 및 변수명 명시적이게 작성 - 주석 간단하게 수정 - Controller 반환형 수정 - TrainingCenter Entity 필드 추가

--- boottalk/src/main/java/com/icandoit/boottalk/bootcamp/entity/TrainingCenter.java
+++ boottalk/src/main/java/com/icandoit/boottalk/bootcamp/entity/TrainingCenter.java
@@ -2,7 +2,6 @@ package com.icandoit.boottalk.bootcamp.entity;
 
 import com.icandoit.boottalk.libs.entity.BaseEntity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,10 +23,20 @@ public class TrainingCenter extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "training_center_id")
 	private Long id;
 
-	@Column(name = "training_center_name", nullable = false)
 	@NotNull(message = "훈련기관 이름은 필수 항목입니다.")
 	private String name;
+
+	@NotNull(message = "훈련기관 연락처는 필수 항목입니다.")
+	private String phoneNumber;
+
+	@NotNull(message = "훈련기관 이메일은 필수 항목입니다.")
+	private String email;
+
+	@NotNull(message = "훈련기관 주소는 필수 항목입니다.")
+	private String address;
+
+	@NotNull(message = "훈련기관 이메일은 필수 항목입니다.")
+	private String url;
 }
