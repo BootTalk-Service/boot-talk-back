1d0c8ce7d432e149fb9511bc2854f8eb56437cbc
park <pty10510@gmail.com>
2025-03-31 00:37:32 +0900

    refactor: Response 수정, Impl 디렉토리 삭제, @Setter 어노테이션 삭제

--- boottalk/src/main/java/com/icandoit/boottalk/bootcamp/repository/BootcampRepository.java
+++
@@ -1,8 +0,0 @@
-package com.icandoit.boottalk.bootcamp.repository;
-
-import com.icandoit.boottalk.bootcamp.entity.Bootcamp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BootcampRepository extends JpaRepository <Bootcamp, Long> {
-
-}
