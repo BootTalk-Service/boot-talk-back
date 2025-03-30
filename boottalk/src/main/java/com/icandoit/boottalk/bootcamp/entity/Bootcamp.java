454ab51a62f032cc3a5a54f025100167643899cf
shCHO9801 <csh980116@naver.com>
2025-03-31 01:47:10 +0900

    refactor : 파일 수정 - Bootcamp, TraningCenter 엔티티 -> column 기본 null 여부만 추가, NotNull 제거 및 칼럼 명 수정 - Controller -> Response 메시지 처리 하던 부분 제거 - ResponseDto -> 단일 from 조회 부분 entity 칼럼 명 수정에 따른 변수명 수정, createdAt과 updatedAt 추가 - Service -> 단일 조회할 때 response를 반환하도록 수정, 카테고리 한국어 변환 메소드 삭제 - Service 단위 테스트 코드 추가 -> 부트캠프 저장, 부트캠프 ID 조회 성공, 실패

--- boottalk/src/main/java/com/icandoit/boottalk/bootcamp/entity/Bootcamp.java
+++ boottalk/src/main/java/com/icandoit/boottalk/bootcamp/entity/Bootcamp.java
@@ -2,7 +2,6 @@ package com.icandoit.boottalk.bootcamp.entity;
 
 import java.time.LocalDate;
 
-import com.icandoit.boottalk.bootcamp.dto.BootcampResponse;
 import com.icandoit.boottalk.libs.entity.BaseEntity;
 
 import jakarta.persistence.Column;
@@ -16,73 +15,71 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "bootcamp")
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "bootcamp")
 public class Bootcamp extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "bootcamp_id")
-	private Long id;
+	@Column(nullable = false)
+	private Long bootcampId;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "training_center_id")
-	@NotNull(message = "트레이닝 센터는 필수 항목입니다.")
+	@JoinColumn(name = "training_center_id", nullable = false)
 	private TrainingCenter trainingCenter;
 
-	@NotNull(message = "부트캠프 이름은 필수 항목입니다.")
+	@Column(nullable = false)
 	private String bootcampName;
 
 	@Enumerated(EnumType.STRING)
-	@NotNull(message = "부트캠프 카테고리는 필수 항목입니다.")
+	@Column(nullable = false)
 	private BootcampCategoryType bootcampCategoryType;
 
-	@NotNull(message = "부트캠프 기수는 필수 항목입니다.")
+	@Column(nullable = false)
 	private int bootcampDegree;
 
-	@NotNull(message = "부트캠프 지역은 필수 항목입니다.")
+	@Column(nullable = false)
 	private String bootcampRegion;
 
-	@NotNull(message = "부트캠프 정원은 필수 항목입니다.")
+	@Column(nullable = false)
 	private int bootcampCapacity;
 
-	@NotNull(message = "부트캠프 비용 정보는 필수 항목입니다.")
+	@Column(nullable = false)
 	private boolean bootcampCost;
 
-	@NotNull(message = "부트캠프 시작일은 필수 항목입니다.")
+	@Column(nullable = false)
 	private LocalDate bootcampStartDate;
 
-	@NotNull(message = "부트캠프 종료일은 필수 항목입니다.")
+	@Column(nullable = false)
 	private LocalDate bootcampEndDate;
 
-	@NotNull(message = "부트캠프 링크는 필수 항목입니다.")
+	@Column(nullable = false)
 	private String bootcampLink;
 
-	// DTO 로부터 Bootcamp 엔티티를 생성하는 메서드
 	public static Bootcamp of(
-		BootcampResponse.BootcampResponseDto dto, TrainingCenter trainingCenter, BootcampCategoryType categoryType
+		TrainingCenter trainingCenter, String name, BootcampCategoryType categoryType, int degree, String region,
+		int capacity, boolean cost, LocalDate startDate, LocalDate endDate, String link
 	) {
 		return Bootcamp.builder()
 			.trainingCenter(trainingCenter)
-			.bootcampName(dto.getBootcampName())
-			.bootcampRegion(dto.getBootcampRegion())
-			.bootcampCost(dto.getBootcampCost())
-			.bootcampLink(dto.getBootcampLink())
+			.bootcampName(name)
 			.bootcampCategoryType(categoryType)
-			.bootcampDegree(dto.getBootcampDegree())
-			.bootcampCapacity(dto.getBootcampCapacity())
-			.bootcampStartDate(dto.getBootcampStartDate())
-			.bootcampEndDate(dto.getBootcampEndDate())
+			.bootcampDegree(degree)
+			.bootcampRegion(region)
+			.bootcampCapacity(capacity)
+			.bootcampCost(cost)
+			.bootcampStartDate(startDate)
+			.bootcampEndDate(endDate)
+			.bootcampLink(link)
 			.build();
 	}
 }
