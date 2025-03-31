4584d1a69bf60dc56fe623d04eb29bc8fb0fe36a
LeeGeonu <rjsdndl909@gmail.com>
2025-03-31 17:36:07 +0900

    feat: 커피챗 시간 관리 등록 기능

---
+++ boottalk/src/main/java/com/icandoit/boottalk/coffeeChat/repository/CoffeeChatTimeRepository.java
@@ -0,0 +1,8 @@
+package com.icandoit.boottalk.coffeeChat.repository;
+
+import com.icandoit.boottalk.coffeeChat.entity.CoffeeChatTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoffeeChatTimeRepository extends JpaRepository<CoffeeChatTime, Long> {
+
+}
