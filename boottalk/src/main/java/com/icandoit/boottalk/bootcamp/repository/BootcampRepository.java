065778165d27aa5f0dd1ee98ff2d47f71ca54866
shCHO9801 <csh980116@naver.com>
2025-04-01 15:54:14 +0900

    feat : 부트캠프 중복 등록 방지 및 존재하지 않는 카테고리 출력 로그 작성

--- boottalk/src/main/java/com/icandoit/boottalk/bootcamp/repository/BootcampRepository.java
+++ boottalk/src/main/java/com/icandoit/boottalk/bootcamp/repository/BootcampRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BootcampRepository extends JpaRepository <Bootcamp, Long> {
 
+	boolean existsByBootcampNameAndBootcampDegree(String bootcampName, int bootcampDegree);
 }
