36b9f5f28e6bb7fd150cd0777b43b617169a07bd
LeeGeonu <rjsdndl909@gmail.com>
2025-04-04 02:06:52 +0900

    feat : 커피챗 시간 수정 API 추가

--- boottalk/src/main/java/com/icandoit/boottalk/coffeeChat/service/CoffeeChatTimeService.java
+++ boottalk/src/main/java/com/icandoit/boottalk/coffeeChat/service/CoffeeChatTimeService.java
@@ -57,7 +57,42 @@ public class CoffeeChatTimeService {
 
         return coffeeChatTimes.stream()
             .map(CoffeeChatTimeResponseDto::from)
-            .collect(Collectors.toList());
+            .toList();
+    }
+
+    @Transactional
+    public List<CoffeeChatTimeResponseDto> updateCoffeeChatTimes(Long mentoId,
+        CoffeeChatTimeListDto requestDto) {
+        // 기존 멘토의 커피챗 시간 조회 (없으면 예외 발생)
+        List<CoffeeChatTime> existingTimeSlots = getMentoCoffeeChatTimesOrThrow(mentoId);
+
+        // 새로운 시간 리스트를 CoffeeChatTime 엔티티 리스트로 변환
+        CoffeeChatInfo coffeeChatInfo = existingTimeSlots.get(0).getCoffeeChatInfo();
+        List<CoffeeChatTime> newTimeSlots = requestDto.availableTimes().stream()
+            .map(timeDto -> CoffeeChatTime.of(coffeeChatInfo, timeDto.dayOfWeek(),
+                timeDto.startTime(), timeDto.endTime()))
+            .toList();
+
+        // 기존과 비교 후 존재하지 않을 경우 toDelete 리스트에 추가
+        List<CoffeeChatTime> toDelete = existingTimeSlots.stream()
+            .filter(
+                existing -> newTimeSlots.stream()
+                    .noneMatch(newSlot -> newSlot.equals(existing)))
+            .toList();
+
+        // 기존과 비교 후 새롭게 추가된 시간을 toAdd 리스트에 추가
+        List<CoffeeChatTime> toAdd = newTimeSlots.stream()
+            .filter(newSlot -> existingTimeSlots.stream()
+                .noneMatch(existing -> existing.equals(newSlot)))
+            .toList();
+
+        // 기존 시간 삭제 및 새로운 시간 추가
+        coffeeChatTimeRepository.deleteAll(toDelete);
+        coffeeChatTimeRepository.saveAll(toAdd);
+
+        return coffeeChatTimeRepository.findAllWithCoffeeChatInfoByUserId(mentoId).stream()
+            .map(CoffeeChatTimeResponseDto::from)
+            .toList();
     }
 
     private List<CoffeeChatTime> getCoffeeChatTimesOrThrow(Long userId) {
