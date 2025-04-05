270f07e68d69e84a588fa2d4e498d1039e0681ed
LeeGeonu <rjsdndl909@gmail.com>
2025-04-05 22:51:57 +0900

    feat : 커피챗 신청 가능시간 조회(일반유저 조회)

--- boottalk/src/main/java/com/icandoit/boottalk/coffeeChat/controller/CoffeeChatTimeController.java
+++ boottalk/src/main/java/com/icandoit/boottalk/coffeeChat/controller/CoffeeChatTimeController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +22,7 @@ public class CoffeeChatTimeController {
 
     private final CoffeeChatTimeService coffeeChatTimeService;
 
-    @PostMapping("/available-times")
+    @PostMapping
     public ResponseEntity<List<CoffeeChatTimeResponseDto>> createCoffeeChatTimes(
         @RequestBody @Valid CoffeeChatTimeMapDto requestDto) {
         // todo : 사용자 인증 사용 시 수정
@@ -29,14 +30,22 @@ public class CoffeeChatTimeController {
         return ResponseEntity.ok(coffeeChatTimeService.createCoffeeChatTimes(userId, requestDto));
     }
 
-    @GetMapping("/my")
+    // 멘토 자신의 시간을 조회
+    @GetMapping
     public ResponseEntity<List<CoffeeChatTimeResponseDto>> getMyCoffeeChatTimes() {
         Long userId = 1L;
 
-        return ResponseEntity.ok(coffeeChatTimeService.getMyCoffeeChatTimes(userId));
+        return ResponseEntity.ok(coffeeChatTimeService.getMentorAvailableChatTimes(userId));
     }
 
-    @PutMapping("/my")
+    //mentorId를 받아 커피챗 가능 시간 조회
+    @GetMapping("/{mentorId}")
+    public ResponseEntity<List<CoffeeChatTimeResponseDto>> getMentorAvailableChatTimes(@PathVariable Long mentorId) {
+
+        return ResponseEntity.ok(coffeeChatTimeService.getMentorAvailableChatTimes(mentorId));
+    }
+
+    @PutMapping
     public ResponseEntity<List<CoffeeChatTimeResponseDto>> updateCoffeeChatTimes(
         @RequestBody @Valid CoffeeChatTimeMapDto requestDto) {
         Long userId = 1L;
