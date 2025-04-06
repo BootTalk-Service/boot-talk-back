0124c15c15660a45ff09148f6dc14b2ba34befa4
pty10510 <pty10510@gmail.com>
2025-04-06 16:00:52 +0900

    feat: createPointHistory 메서드에 @Transactional 어노테이션 추가, 현재 포인트 데이터 가져올 때 비관적 락 사용

--- boottalk/src/main/java/com/icandoit/boottalk/point_history/domain/repository/PointHistoryRepository.java
+++ boottalk/src/main/java/com/icandoit/boottalk/point_history/domain/repository/PointHistoryRepository.java
@@ -5,12 +5,16 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import com.icandoit.boottalk.point_history.domain.entity.PointHistory;
+
+import jakarta.persistence.LockModeType;
 
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
 
 	Page<PointHistory> findAllByUserId(long userId, Pageable pageable);
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	Optional<PointHistory> findTopByUserIdOrderByPointHistoryIdDesc(long userId);
 }
