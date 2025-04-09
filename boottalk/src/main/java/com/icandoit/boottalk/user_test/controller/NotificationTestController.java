319d4faf902be627d97b097fc98b75abaf11ccee
pty10510 <pty10510@gmail.com>
2025-04-09 10:47:10 +0900

    feat: 내 알림 조회 기능, 알림 일괄 확인 처리 기능

--- boottalk/src/main/java/com/icandoit/boottalk/user_test/controller/NotificationTestController.java
+++ boottalk/src/main/java/com/icandoit/boottalk/user_test/controller/NotificationTestController.java
@@ -1,5 +1,6 @@
 package com.icandoit.boottalk.user_test.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,6 @@ import com.icandoit.boottalk.notification.dto.NotificationRequestDto;
 import com.icandoit.boottalk.notification.dto.NotificationResponseDto;
 import com.icandoit.boottalk.notification.service.NotificationService;
 import com.icandoit.boottalk.notification.service.SseEmitterService;
-import com.icandoit.boottalk.social_login.jwt.JwtProvider;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,14 +24,15 @@ public class NotificationTestController {
 	private final NotificationService notificationService;
 	private final SseEmitterService emitterService;
 
-	@PostMapping("/sse-endpoint")
-	public ResponseEntity<SseEmitter> connect(@RequestParam long userId) {
-		return ResponseEntity.ok(emitterService.subscribe(userId));
+	@PostMapping(value = "/sse-endpoint", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public SseEmitter connect(@RequestParam long userId) {
+
+		return emitterService.subscribe(userId);
 	}
 
 
 	@PostMapping
 	public ResponseEntity<NotificationResponseDto>  create(@RequestParam long userId, @RequestBody NotificationRequestDto dto) {
-		return ResponseEntity.ok(notificationService.sendNotification(userId, dto));
+		return ResponseEntity.ok(notificationService.sendLiveNotification(userId, dto));
 	}
 }
