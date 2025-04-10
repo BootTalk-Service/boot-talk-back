7315e948c7f8635d5b10845d2a7a561506ab1d4f
LeeGeonu <rjsdndl909@gmail.com>
2025-04-10 14:35:26 +0900

    fix : UserEntity 관련한 파일 제거

--- boottalk/src/main/java/com/icandoit/boottalk/stomp_chat/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.icandoit.boottalk.stomp_chat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.icandoit.boottalk.stomp_chat.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
