70ca9be27844bd2592505580c2bdd3346f453770
pty10510 <pty10510@gmail.com>
2025-04-15 23:55:09 +0900

    feat:프론트엔드 연동을 위한 SecurityFilter Cors 추가, loginPage() 삭제

--- boottalk/src/main/java/com/icandoit/boottalk/social_login/config/SecurityConfiguration.java
+++ boottalk/src/main/java/com/icandoit/boottalk/social_login/config/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.icandoit.boottalk.social_login.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -9,6 +12,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.icandoit.boottalk.social_login.jwt.JwtFilter;
 import com.icandoit.boottalk.social_login.oauth2.CustomClientRegistrationRepository;
@@ -33,6 +39,7 @@ public class SecurityConfiguration {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http
+			.cors(cors ->  cors.configurationSource(corsConfigurationSource()))
 			.csrf(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
@@ -42,12 +49,11 @@ public class SecurityConfiguration {
 			.authorizeHttpRequests(auth -> auth
 				// 경로에 대한 접근 권한 설정
 				// TODO  : 추후에 접근 가능한 페이지 설정
-				.requestMatchers("/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+				.requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**", "/api/oauth2/authorization/**").permitAll()
 				.anyRequest().authenticated())
 			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 			//oauth2 로그인 설정
 			.oauth2Login(oauth2 -> oauth2
-				.loginPage("http://localhost:3000/login") // 사용자 인증이 필요한 경우 해당 페이지로 이동함.
 				.authorizationEndpoint(endpoint -> endpoint. // 프론트엔드쪽에서 로그인 버튼을 눌렀을 때
 					baseUri("/api/oauth2/authorization/naver")) // 해당 엔드포인트로 요청을 보내면 네이버 로그인 페이지로 리디렉션
 				.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
@@ -60,7 +66,17 @@ public class SecurityConfiguration {
 				})
 				.clearAuthentication(true));
 
-
 		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
