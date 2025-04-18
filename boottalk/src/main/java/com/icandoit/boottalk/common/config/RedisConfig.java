9a0ebf4636b69612ce8da19241ebafe9de5b6efc
LeeGeonu <rjsdndl909@gmail.com>
2025-04-18 18:16:14 +0900

    fix : redis 직렬화 수정

--- boottalk/src/main/java/com/icandoit/boottalk/common/config/RedisConfig.java
+++ boottalk/src/main/java/com/icandoit/boottalk/common/config/RedisConfig.java
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.icandoit.boottalk.stomp_chat.dto.ChatMessageResponseDto;
 import com.icandoit.boottalk.stomp_chat.dto.ChatRoomResponseDto;
-import io.lettuce.core.ReadFrom;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -28,26 +26,11 @@ public class RedisConfig {
     private int redisPort;
 
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        return objectMapper;
-    }
-
-    @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        // Redis 서버 설정
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
-
-        // Lettuce 클라이언트 설정 - 읽기는 레플리카 우선, 쓰기는 마스터로 자동 라우팅
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-            .readFrom(ReadFrom.REPLICA_PREFERRED)
-            .build();
-
-        return new LettuceConnectionFactory(config, clientConfig);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost,
+            redisPort);
+        return new LettuceConnectionFactory(config);
     }
-
 
     @Bean
     public StringRedisTemplate stringRedisTemplate(
@@ -56,39 +39,49 @@ public class RedisConfig {
     }
 
     @Bean
+    public GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return new GenericJackson2JsonRedisSerializer(objectMapper);
+    }
+
+    @Bean
     public RedisTemplate<String, ChatMessageResponseDto> chatMessageDtoRedisTemplate(
-        LettuceConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        LettuceConnectionFactory redisConnectionFactory,
+        GenericJackson2JsonRedisSerializer serializer) {
 
         RedisTemplate<String, ChatMessageResponseDto> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setValueSerializer(serializer);
 
         return template;
     }
 
     @Bean
     public RedisTemplate<String, ChatRoomResponseDto> chatRoomDtoRedisTemplate(
-        LettuceConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        LettuceConnectionFactory redisConnectionFactory,
+        GenericJackson2JsonRedisSerializer serializer) {
 
         RedisTemplate<String, ChatRoomResponseDto> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setValueSerializer(serializer);
 
         return template;
     }
 
     @Bean
     public RedisTemplate<String, Boolean> booleanRedisTemplate(
-        RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        RedisConnectionFactory redisConnectionFactory,
+        GenericJackson2JsonRedisSerializer serializer) {
+
         RedisTemplate<String, Boolean> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setValueSerializer(serializer);
+
         return template;
     }
-
-
 }
-
