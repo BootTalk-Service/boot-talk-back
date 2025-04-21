c61d39acbe0d4541c4e71251be87a6e232e83a57
pty10510 <pty10510@gmail.com>
2025-04-21 22:50:05 +0900

    사용하지않는 import문 삭제

--- boottalk/src/main/java/com/icandoit/boottalk/notification/controller/SseController.java
+++ boottalk/src/main/java/com/icandoit/boottalk/notification/controller/SseController.java
@@ -1,13 +1,10 @@
 package com.icandoit.boottalk.notification.controller;
 
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
