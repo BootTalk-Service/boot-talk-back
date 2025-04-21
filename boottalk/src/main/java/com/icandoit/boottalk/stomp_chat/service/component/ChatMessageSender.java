56a96cc48d1c16ebc78ce5cd2ecec8311a533389
LeeGeonu <rjsdndl909@gmail.com>
2025-04-22 04:13:52 +0900

    refactor : 사용하지 않는 주석 제거 및 로직설명 주석추가

--- boottalk/src/main/java/com/icandoit/boottalk/stomp_chat/service/component/ChatMessageSender.java
+++ boottalk/src/main/java/com/icandoit/boottalk/stomp_chat/service/component/ChatMessageSender.java
@@ -26,6 +26,7 @@ public class ChatMessageSender {
     // 입장 메시지 전송
     public void sendEnterMessage(Long userId, String roomUuid) {
         // Redis에서 사용자가 입장했는지 확인
+        // 있다면 return ( 정책상 입장 메시지 1회 전송)
         if (!redisChatUserRepository.hasUserEntered(roomUuid, userId)) {
             return;
         }
@@ -41,7 +42,6 @@ public class ChatMessageSender {
             roomUuid, 0L, receiverId, "님이 입장하였습니다.", MessageType.SYSTEM, LocalDateTime.now(), false
         );
 
-        // 공통화된 메시지 전송 메서드 사용
         sendMessage(receiverId, enterMessage);
     }
 
