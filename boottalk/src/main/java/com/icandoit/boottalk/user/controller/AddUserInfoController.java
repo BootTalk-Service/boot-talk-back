950c0be75d1cdfac06368fa8182a7ef148939bfb
pty10510 <pty10510@gmail.com>
2025-04-22 20:04:57 +0900

    refactor: 회원가입 추가 정보 입력 엔드포인트 수정

--- boottalk/src/main/java/com/icandoit/boottalk/user/controller/AddUserInfoController.java
+++ boottalk/src/main/java/com/icandoit/boottalk/user/controller/AddUserInfoController.java
@@ -16,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users/complete-signup")
+@RequestMapping("/api/users/extra")
 public class AddUserInfoController {
 
   private final AddUserInfoService addUserInfoService;
