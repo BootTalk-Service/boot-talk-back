beda7b45b24332cc88c1517a7e7bbaf8a24852a6
LeeGeonu <rjsdndl909@gmail.com>
2025-04-23 16:22:08 +0900

    feat : chatroom job config 추가

---
+++ boottalk/src/main/java/com/icandoit/boottalk/stomp_chat/config/QuartzConfig.java
@@ -0,0 +1,38 @@
+package com.icandoit.boottalk.stomp_chat.config;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+@Configuration
+public class QuartzConfig {
+
+    private final AutowireCapableBeanFactory beanFactory;
+
+    public QuartzConfig(AutowireCapableBeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setJobFactory(springBeanJobFactory());
+        return factory;
+    }
+
+    @Bean
+    public SpringBeanJobFactory springBeanJobFactory() {
+        return new SpringBeanJobFactory() {
+            @Override
+            protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+                Object job = super.createJobInstance(bundle);
+                beanFactory.autowireBean(job);
+                return job;
+            }
+        };
+    }
+}
+
